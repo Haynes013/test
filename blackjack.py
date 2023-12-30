922169a472d744cb2ff1035cb5d10b0e3ce7de77
Mark Haynes Jr <mhaynes031395@gmail.com>
2023-12-30 13:13:32 -0500

    add calculate score function

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,7 @@
 import random
 
+game_continue = True
+
 def deal_card():
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
     return random.choice(cards)
@@ -10,7 +12,13 @@ for card in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
   
-def calculate_score(cards):
-    return sum(cards)
+def calculate_score(hand_cards):
+    hand = sum(hand_cards)
+    if hand == 21 and len(hand_cards) == 2:
+        hand = 0
+    elif hand > 21 and 11 in hand_cards:
+        hand_cards[hand_cards.index(11)] = 1
+        hand = sum(hand_cards)
+    return hand 
 
   
